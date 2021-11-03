ba40ec65d9798175ba8375caba19459b51120ede
hwar042 <hwar042@aucklanduni.ac.nz>
2021-11-03 15:49:11 +1300

    Changed graph database, files output to single folder

--- src/slackCalculationScript.java
+++ src/slackCalculationScript.java
@@ -1,22 +1,22 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+
+import static org.apache.commons.io.FileUtils.listFiles;
 
 public class slackCalculationScript {
 
     public static void main(String[] args) {
 
-        multipleScheduleCalculate("src/graphs/");
+        multipleScheduleCalculate("src/graphs/BenchmarkSet");
+        multipleScheduleCalculate("src/graphs/listSchedVSclusterSched");
     }
 
     private static void multipleScheduleCalculate(String folderPath) {
         // Get GXL Files in folder
-        File[] graphTypes = new File(folderPath).listFiles();
-        ArrayList<File> gxlFiles = new ArrayList<>();
-        for (File file : graphTypes) {
-            gxlFiles.addAll(Arrays.asList(file.listFiles()));
-        }
+        ArrayList<File> gxlFiles;
+        gxlFiles = (ArrayList<File>) listFiles(new File(folderPath), new String[] {"gxl"},true);
+
         // Calculate Slack for each file
         Runtime rt = Runtime.getRuntime();
         int[] numProcessors = new int[]{2, 4, 8, 20};
@@ -29,12 +29,10 @@ public class slackCalculationScript {
 
         for (File file : gxlFiles) {
             long startTime = System.currentTimeMillis();
-            File outputDir = new File(file.getParent() + "/output/");
-            outputDir.mkdir();
             for (int proc : numProcessors) {
                 for (String priority : priorities) {
                     for (String placement : placements) {
-                        String outputFileName = file.getParent() + "/output/" + file.getName() + proc + priority + placement + ".gxl";
+                        String outputFileName = "output/" + file.getName() + proc + priority + placement + ".gxl";
                         if (!new File(outputFileName).exists()) {
                             try {
                                 rt.exec("java -jar parcschedule-1.0-jar-with-dependencies.jar -f " + file.getAbsolutePath() + " -o " + outputFileName + " -p " + proc + " -priority " + priority + " -placement " + placement).waitFor();
@@ -47,13 +45,11 @@ public class slackCalculationScript {
                 for (String clusterer : clusterers) {
                     for (String merger : mergers) {
                         for (String ordering : orderings) {
-                            String outputFileName = file.getParent() + "/output/" + file.getName() + proc + clusterer + merger + ordering + ".gxl";
+                            String outputFileName = "output/" + file.getName() + proc + clusterer + merger + ordering + ".gxl";
                             if (!new File(outputFileName).exists()) {
                                 try {
                                     rt.exec("java -jar parcschedule-1.0-jar-with-dependencies.jar -f " + file.getAbsolutePath() + " -o " + outputFileName + " -p " + proc + " -clusterer " + clusterer + " -merger " + merger + " -ordering " + ordering).waitFor();
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                } catch (IOException e) {
+                                } catch (InterruptedException | IOException e) {
                                     e.printStackTrace();
                                 }
                             }
